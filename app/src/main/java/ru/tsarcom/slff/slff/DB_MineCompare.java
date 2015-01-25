21f3b1fc3ed9966a832e90f584df0285549709de
Vladislav <vlad.tsaran@gmil.com>
2015-01-25 17:07:39 +0700

    delete old comments

--- app/src/main/java/ru/tsarcom/slff/slff/DB_MineCompare.java
+++ app/src/main/java/ru/tsarcom/slff/slff/DB_MineCompare.java
@@ -9,19 +9,20 @@ import android.database.sqlite.SQLiteOpenHelper;
 /**
  * Created by USRSLM on 18.01.2015.
  */
-public class DB {
+public class DB_MineCompare {
 
-    private static final String DB_NAME = "SelfyTest2";
-    private static final int DB_VERSION = 4;
+    private static final String DB_NAME = "SelfyTest3";
+    private static final int DB_VERSION = 1;
 
     private static final String DBT_MC = "my_compare";
+
     public static final String C_MC_ID = "_id";
     public static final String C_MC_CID = "cid";
     public static final String C_MC_DATE_CRT = "date_crt";
     public static final String C_MC_VOITE_LEFT = "voite_left";
     public static final String C_MC_VOITE_RIGHT = "voite_right";
-    public static final String C_MC_PHOTO_LEFT = "imgOrientation_left";
-    public static final String C_MC_PHOTO_RIGHT = "imgOrientation_right";
+    public static final String C_MC_PHOTO_LEFT = "img_left";
+    public static final String C_MC_PHOTO_RIGHT = "img_right";
     public static final String C_MC_ORNT_LEFT = "imgOrientation_left";
     public static final String C_MC_ORNT_RIGHT = "imgOrientation_right";
     public static final String C_MC_STATUS = "status";
@@ -42,29 +43,13 @@ public class DB {
                     "); ";
 
 
-    public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_IMG = "img";
-    public static final String COLUMN_TXT = "txt";
-    public static final String COLUMN_TXT2 = "txt2";
-    public static final String COLUMN_I = "i";
-
-    private static final String DB_TABLE = "mytab";
-    private static final String DB_CREATE =
-            "create table " + DB_TABLE + "(" +
-                    COLUMN_ID + " integer primary key autoincrement, " +
-                    COLUMN_IMG + " integer, " +
-                    COLUMN_TXT + " text," +
-                    COLUMN_TXT2 + " text," +
-                    COLUMN_I + " text" +
-                    "); ";
-
 
     private final Context mCtx;
 
     private DBHelper mDBHelper;
     private SQLiteDatabase mDB;
 
-    public DB(Context ctx) {
+    public DB_MineCompare(Context ctx) {
         mCtx = ctx;
     }
 
@@ -81,17 +66,7 @@ public class DB {
 
     // получить все данные из таблицы DBT_MC
     public Cursor getAllDataMC() {
-        return mDB.query(DBT_MC, null,C_MC_ID+ " > "+this.getLastIdMC(), null, null, null, null);
-    }
-    // получить max id из таблицы DBT_MC
-    public String getLastIdMC() {
-        Cursor cursor = mDB.rawQuery("SELECT MAX("+C_MC_ID+") as "+C_MC_ID+" FROM "+DBT_MC, null);
-        return cursor.getString(cursor.getColumnIndex(C_MC_ID));
-    }
-
-    // получить все данные из таблицы DB_TABLE
-    public Cursor getAllData() {
-        return mDB.query(DB_TABLE, null, null, null, null, null, null);
+        return mDB.query(DBT_MC, null,null, null, null, null, null);
     }
 
     // добавить запись в DBT_MC
@@ -103,10 +78,10 @@ public class DB {
         cv.put(C_MC_DATE_CRT, DATE_CRT);
         cv.put(C_MC_VOITE_LEFT, VOITE_LEFT);
         cv.put(C_MC_VOITE_RIGHT, VOITE_RIGHT);
-        cv.put(C_MC_VOITE_RIGHT, PHOTO_LEFT);
-        cv.put(C_MC_VOITE_RIGHT, PHOTO_RIGHT);
-        cv.put(C_MC_VOITE_RIGHT, ORNT_LEFT);
-        cv.put(C_MC_VOITE_RIGHT, ORNT_RIGHT);
+        cv.put(C_MC_PHOTO_LEFT, PHOTO_LEFT);
+        cv.put(C_MC_PHOTO_RIGHT, PHOTO_RIGHT);
+        cv.put(C_MC_ORNT_LEFT, ORNT_LEFT);
+        cv.put(C_MC_ORNT_RIGHT, ORNT_RIGHT);
         cv.put(C_MC_STATUS, STATUS);
         mDB.insert(DBT_MC, null, cv);
     }
@@ -125,35 +100,19 @@ public class DB {
         @Override
         public void onCreate(SQLiteDatabase db) {
             // создаем БД
-            db.execSQL(DB_CREATE);
             db.execSQL(DB_MC_CREATE);
 
-//            db.execSQL(DB_MA_CREATE);
-//            db.execSQL(DB_OC_CREATE);
-//            db.execSQL(DB_PHOTO_CREATE);
-
             // заполняем БД
-            ContentValues cv = new ContentValues();
-            for (int i = 1; i < 10; i++) {
-                addRecMC(i,"2015-01-25 "+i,1,1,R.drawable.ic_launcher,R.drawable.ic_launcher,0,0,0);
-                cv.put(COLUMN_I,  "# "+ i + " - ");
-                cv.put(COLUMN_TXT,   " - тест DB ver. ");
-                cv.put(COLUMN_TXT2,   " " + DB_VERSION);
-                cv.put(COLUMN_IMG, R.drawable.ic_launcher);
-                db.insert(DB_TABLE, null, cv);
-            }
+//            for (int i = 1; i < 10; i++) {
+//                addRecMC(i,"2015-01-25 "+i,1,1,R.drawable.ic_launcher,R.drawable.ic_launcher,0,0,0);
+//            }
 
         }
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
-            db.execSQL(" DROP TABLE IF EXISTS " + DB_TABLE+"; ");
             db.execSQL(" DROP TABLE IF EXISTS " + DBT_MC+"; ");
-
-//            db.execSQL("DROP TABLE IF EXISTS " + DBT_MA);
-//            db.execSQL("DROP TABLE IF EXISTS " + DBT_OC);
-//            db.execSQL("DROP TABLE IF EXISTS " + DBT_PHOTO);
             onCreate(db);
         }
     }
--- app/src/main/java/ru/tsarcom/slff/slff/MineViewsActivity.java
+++ app/src/main/java/ru/tsarcom/slff/slff/MineViewsActivity.java
@@ -1,6 +1,7 @@
 package ru.tsarcom.slff.slff;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -101,7 +102,7 @@ public class MineViewsActivity extends FragmentActivity implements LoaderCallbac
 
     private static final int CM_DELETE_ID = 1;
     ListView lvData;
-    DB db;
+    DB_MineCompare db_MC;
     SimpleCursorAdapter scAdapter;
 
     @Override
@@ -113,20 +114,16 @@ public class MineViewsActivity extends FragmentActivity implements LoaderCallbac
         id_account = intent.getStringExtra("id_account");
 
         String fileName = intent.getStringExtra(FILE_NAME);
-//                Toast.makeText(getBaseContext(), "12 id_account = " + id_account + " ;", Toast.LENGTH_LONG).show();
-//                Toast.makeText(getBaseContext(), "22 fileName = - " + fileName + " ;", Toast.LENGTH_LONG).show();
         activity = this;
-//        imagetest = (ImageView) findViewById(R.id.imageView);
         if(isConnected()) {
 
             String url0;
             url0 = "http://95.78.234.20:81/atest/jsonMine.php?id_account="+id_account;
-            new HttpAsyncTask().execute(url0);
+//            new HttpAsyncTask().execute(url0);
         }else{
 
                    Toast.makeText(getBaseContext(), "Нет Соединения с интернетом :(", Toast.LENGTH_LONG).show();
         }
-
 
         tvNickname = (TextView)findViewById(R.id.tvNickname);
         tvNickname.setText(id_account);
@@ -135,9 +132,6 @@ public class MineViewsActivity extends FragmentActivity implements LoaderCallbac
         btnOthers.setOnClickListener(this);
         btnCreate = (Button) findViewById(R.id.btnCreate);
         btnCreate.setOnClickListener(this);
-//        btnSertviceStart = (Button) findViewById(R.id.btnSertviceStart);
-//        btnSertviceStart.setOnClickListener(this);
-
 
         // находим список
         lvSimple = (ListView) findViewById(R.id.lvSimple);
@@ -145,57 +139,35 @@ public class MineViewsActivity extends FragmentActivity implements LoaderCallbac
         // для tvColor и tvSize необходимо создавать контекстное меню
         registerForContextMenu(lvSimple);
 
-
-//        btnCreateI = (Button) findViewById(R.id.btnCreateI);
-//        btnCreateI.setOnClickListener(this);
-
-
         lvSimple.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
 
                 TextView tvCid = (TextView) view.findViewById(R.id.tvCid);
                 String strCid = tvCid.getText().toString();
-
-//                Toast.makeText(getBaseContext(), "itemClick: position = " + position + ", id = "
-//                        + id+" "+strCid, Toast.LENGTH_LONG).show();
-
                 Intent intent = new Intent(activity, ViewActivity.class);
                 intent.putExtra("id_account", id_account);
                 intent.putExtra("id_compare", strCid);
                 startActivity(intent);
             }
         });
-//
-//        lvMain.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            public void onItemSelected(AdapterView<?> parent, View view,
-//                                       int position, long id) {
-//
-//            }
-//
-//            public void onNothingSelected(AdapterView<?> parent) {
-//
-//                   Toast.makeText(getBaseContext(), "itemSelect: nothing", Toast.LENGTH_LONG).show();
-////                Log.d(LOG_TAG, "itemSelect: nothing");
-//            }
-//        });
-
 
 //        // открываем подключение к БД
-        db = new DB(this);
-        db.open();
+        db_MC = new DB_MineCompare(this);
+        db_MC.open();
 
         // формируем столбцы сопоставления
-        String[] from = new String[] { DB.COLUMN_IMG, DB.COLUMN_TXT };
-        int[] to = new int[] { R.id.ivImg, R.id.tvText };
 
-        // создааем адаптер и настраиваем список
-        scAdapter = new SimpleCursorAdapter(this, R.layout.item_test, null, from, to, 0);
-        lvSimple.setAdapter(scAdapter);
-
-
-        // создаем лоадер для чтения данных
-        getSupportLoaderManager().initLoader(0, null, this);
+//        String[] from = new String[] { DB_MineCompare.C_MC_CID,DB_MineCompare.C_MC_PHOTO_LEFT ,DB_MineCompare.C_MC_PHOTO_RIGHT, DB_MineCompare.C_MC_DATE_CRT };
+//        int[] to = new int[] { R.id.tvCid, R.id.ivLeft,R.id.ivRight, R.id.tvCdate };
+//
+//        // создааем адаптер и настраиваем список
+//        scAdapter = new MySimpleCursorAdapter(this, R.layout.item_test, null, from, to, 0);
+//        lvSimple.setAdapter(scAdapter);
+////
+////
+////        // создаем лоадер для чтения данных
+//        getSupportLoaderManager().initLoader(0, null, this);
 
 
     }
@@ -364,14 +336,14 @@ public class MineViewsActivity extends FragmentActivity implements LoaderCallbac
                         lvSimple.setAdapter(sAdapter);
 
 
-                        // формируем столбцы сопоставления
-                        String[] from_c = new String[] { DB.COLUMN_IMG, DB.COLUMN_TXT };
-//                        int[] to = new int[] { R.id.ivImg, R.id.tvText };
-
-                        // создааем адаптер и настраиваем список
-                        scAdapter = new SimpleCursorAdapter(activity, R.layout.mine_list_view, null, from, to, 0);
-//                        lvData = (ListView) findViewById(R.id.lvData);
-                        lvSimple.setAdapter(scAdapter);
+//                        // формируем столбцы сопоставления
+//                        String[] from_c = new String[] { DB.COLUMN_IMG, DB.COLUMN_TXT };
+////                        int[] to = new int[] { R.id.ivImg, R.id.tvText };
+//
+//                        // создааем адаптер и настраиваем список
+//                        scAdapter = new SimpleCursorAdapter(activity, R.layout.mine_list_view, null, from, to, 0);
+////                        lvData = (ListView) findViewById(R.id.lvData);
+//                        lvSimple.setAdapter(scAdapter);
     //                    JSONObject joCompare = jaCompare.getJSONObject(0);
     //                    String date_crt = joCompare.getString("date_crt");
                     }
@@ -442,6 +414,10 @@ public class MineViewsActivity extends FragmentActivity implements LoaderCallbac
                 } catch(NumberFormatException nfe) {
                     System.out.println("Could not parse " + imgOrientation_right);
                 }
+//            Toast.makeText(getBaseContext(), "imgOrientation_left = "+imgOrientation_left,
+//                    Toast.LENGTH_SHORT).show();
+//            Toast.makeText(getBaseContext(), "imgOrientation_right = "+imgOrientation_right,
+//                    Toast.LENGTH_SHORT).show();
                 int iall_voiteLeft = 1;
                 try {
                     iall_voiteLeft = Integer.parseInt(all_voiteLeft);
@@ -464,6 +440,12 @@ public class MineViewsActivity extends FragmentActivity implements LoaderCallbac
 //                pbLoad.setProgress(5);
 
                 int img = R.drawable.abc_ic_clear_mtrl_alpha;
+//                if (url_left.equals("empty")) {
+//                    url_left = "empty.png";
+//                }
+//                if (url_right.equals("empty")) {
+//                    url_right = "empty.png";
+//                }
                 ImageLoaderSmall.DisplayImage("http://95.78.234.20:81/atest/"+url_left, img, ivLeft, imgOrientation_left_i);
                 ImageLoaderSmall.DisplayImage("http://95.78.234.20:81/atest/"+url_right, img, ivRight, imgOrientation_right_i);
                 return vi;
@@ -472,20 +454,47 @@ public class MineViewsActivity extends FragmentActivity implements LoaderCallbac
 
 
 
-        public class MySimpleCursorAdapter extends SimpleCursorAdapter{
-            public MySimpleCursorAdapter(Context context, int layout, Cursor c, String[] from, int[] to) {
-                super(context, layout, c, from, to);
+        /*
+        class MySimpleAdapter2 extends SimpleAdapter {
+
+            ArrayList<MineCompare> objects;
+            private Context mContext;
+            public ImageLoader imageLoader;
+            public LayoutInflater inflater=null;
+            public MySimpleAdapter2(Context context,
+                                    ArrayList<MineCompare> MineCompares, int resource,
+                                   String[] from, int[] to) {
+                super(context, objects, resource, from, to);
+                mContext = context;
+                inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                imageLoader=new ImageLoader(mContext.getApplicationContext());
             }
 
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
-                View view = super.getView(position, convertView, parent);
-                long id=getItemId(position);
-                view.setTag(id);
-                return view;
+//                View vi=convertView;
+//                if(convertView==null)
+//                    vi = inflater.inflate(R.layout.mine_list_view, null);
+//
+//                HashMap<String, Object> data = (HashMap<String, Object>) getItem(position);
+//                TextView tvCid = (TextView)vi.findViewById(R.id.tvCid);
+//                TextView tvCdate = (TextView)vi.findViewById(R.id.tvCdate);
+//                ImageView ivLeft=(ImageView)vi.findViewById(R.id.ivLeft);
+//                ImageView ivRight=(ImageView)vi.findViewById(R.id.ivRight);
+//                String strCid = (String) data.get(ATTRIBUTE_COMPARE_ID);
+//                String strCdate = (String) data.get(ATTRIBUTE_NAME_DATE_CRT);
+//                String url_left = (String) data.get(ATTRIBUTE_NAME_LEFT);
+//                String url_right = (String) data.get(ATTRIBUTE_NAME_RIGHT);
+//                tvCid.setText(strCid);
+//                tvCdate.setText(strCdate);
+//
+//                int img = R.drawable.abc_ic_clear_normal;
+//                imageLoader.DisplayImage(url_left, img, ivLeft);
+//                imageLoader.DisplayImage(url_right, img, ivRight);
+//                return vi;
             }
         }
-
+        */
     }
 
 
@@ -640,12 +649,12 @@ public class MineViewsActivity extends FragmentActivity implements LoaderCallbac
     protected void onDestroy() {
         super.onDestroy();
         // закрываем подключение при выходе
-        db.close();
+        db_MC.close();
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle bndl) {
-        return new MyCursorLoader(this, db);
+        return new MyCursorLoader(this, db_MC);
     }
 
     @Override
@@ -659,16 +668,16 @@ public class MineViewsActivity extends FragmentActivity implements LoaderCallbac
 
     static class MyCursorLoader extends CursorLoader {
 
-        DB db;
+        DB_MineCompare db_MC_;
 
-        public MyCursorLoader(Context context, DB db) {
+        public MyCursorLoader(Context context, DB_MineCompare db_MC) {
             super(context);
-            this.db = db;
+            this.db_MC_ = db_MC;
         }
 
         @Override
         public Cursor loadInBackground() {
-            Cursor cursor = db.getAllData();
+            Cursor cursor = db_MC_.getAllDataMC();
             try {
                 TimeUnit.SECONDS.sleep(3);
             } catch (InterruptedException e) {
@@ -678,4 +687,48 @@ public class MineViewsActivity extends FragmentActivity implements LoaderCallbac
         }
 
     }
+
+
+    public class MySimpleCursorAdapter extends SimpleCursorAdapter{
+        private Context mContext;
+        public ImageLoaderSmall ImageLoaderSmall;
+        public Cursor  cur_t;
+        public LayoutInflater inflater=null;
+        public MySimpleCursorAdapter(Context context, int layout, Cursor c, String[] from, int[] to, int flags)
+        {
+            super(context, layout, c, from, to,0);
+            mContext = context;
+            this.cur_t = c;
+            inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            ImageLoaderSmall=new ImageLoaderSmall(mContext.getApplicationContext());
+
+//            this.cur_t.moveToFirst();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+//                View view = super.getView(position, convertView, parent);
+//                long id=getItemId(position);
+//                view.setTag(id);
+//                return view;
+            View vi=convertView;
+            if(convertView==null)
+                vi = inflater.inflate(R.layout.mine_list_view, null);
+            Cursor cur_i;
+            cur_i = this.cur_t;
+//            int cnt = cur_i.getCount();
+//            final int rowID = cur_t.getInt(cur_t.getColumnIndex("_id"));  //Добавил это
+//            this.cur_t.moveToPosition(position);
+//            int rowID = this.cur_t.getInt(cur_t.getColumnIndex("img"));  //Добавил это
+//            this.cur_t.moveToPosition(position);
+//                HashMap<String, Object> data = (HashMap<String, Object>) getItem(position);
+                TextView tvCid = (TextView)vi.findViewById(R.id.tvCid);
+                String strCid = "9999";
+                tvCid.setText(strCid);
+
+            return vi;
+        }
+    }
+
+
 }
